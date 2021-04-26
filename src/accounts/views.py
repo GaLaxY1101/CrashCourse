3e41f0c40c1dd20effbde1ebf5da6fc07c67ae6d
Illua <korniykhik3@gmail.com>
2021-04-26 14:41:33 +0300

    Add Inline Formsets into create order

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory # нужно для multiply form
 from .models import *
 from .forms import OrderForm
 
@@ -35,18 +36,20 @@ def customer(request, pk):
 		'orders_c':orders_c,
 		})	
 
-def create_order(request):
-
-	form = OrderForm() 
+def create_order(request, pk):
+	OrderFormSet = inlineformset_factory(Customer, Order,fields=('product','status'),extra=5) #екстра - количетсов форм
+	customer = Customer.objects.get(id=pk)
+	formset = OrderFormSet(queryset=Order.objects.none(),instance=customer)
+	#form = OrderForm(initial={'customer':customer}) # Initial вставляет  данные из в форму по умолчанию
 	if request.method == 'POST':
 		#print('Printing POST method',request)
-		form = OrderForm(request.POST)
-		if form.is_valid():
-			form.save()
+		formset = OrderFormSet(request.POST,instance=customer)
+		if formset.is_valid():
+			formset.save()
 			return redirect('/')
 
 
-	context = {'form':form}
+	context = {'formset':formset}
 	return render(request, 'accounts/order_form.html',context)
 
 def update_order(request, order_pk):
